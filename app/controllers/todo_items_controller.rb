88a52fe1cd1c2b5f91848dc4cd8623b15b5f5292
wtchang92 <Wtchang@Tims-MacBook-Pro.local>
2015-01-08 23:03:05 -0500

    added edit todo_items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -18,6 +18,28 @@ class TodoItemsController < ApplicationController
       flash[:error]  = "There was a problem adding the list item" 
       render action: :new
     end
+  end
+  
+  def edit
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+  
+  def update
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.save
+      flash[:success] = "Save todo list item"
+      redirect_to todo_list_todo_items_path
+    else
+      flash[:error]  = "That todo item could not be saved" 
+      render action: :edit
+    end
+  end
+  
+  def url_options
+    { todo_list_id: params[:todo_list_id] }.merge(super)
+  end
   
   private
   def todo_item_params
